639f0bbb681ab63d2aa257cd886004a89ac1addf
fabihasohail <msohailfabiha@gmail.com>
2026-08-22 15:51:13 +0500

    commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const supabaseUrl = "https://wmflitippmldqpfvxixt.supabase.co";
+const supabaseKey = "sb_publishable_0VvRE72u8ZALMxvFXcuC5w_vAFO97BS";
+
+
+const { createClient } = supabase;
+const client = createClient(supabaseUrl, supabaseKey);
