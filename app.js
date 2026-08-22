fcb6d06511dd59717157c2e31630939581350758
fabihasohail <msohailfabiha@gmail.com>
2026-08-22 15:57:38 +0500

    button

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const supabaseKey = "sb_publishable_0VvRE72u8ZALMxvFXcuC5w_vAFO97BS";
 
 const { createClient } = supabase;
 const client = createClient(supabaseUrl, supabaseKey);
+
+let subBtn = document.querySelector("#btn");
+subBtn.addEventListener("click",()=>{
+    console.log("okkkkkkkkk");
+})
